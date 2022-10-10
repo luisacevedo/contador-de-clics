90521e33f638b38cbe31f1674455d544e2d42fac
acevedoluise <lea0133@hotmail.com>
2022-10-10 18:50:03 -0500

    Second commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/Boton.js';
+import freeCodeCampLogo from './pictures/FreeCodeCamp_logo.svg'
 
 function App() {
+
+  const manejarClick = () => {
+    console.log("Click"); 
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+        texto='Click'
+        esBotonDeClick={ true }
+        manejarClick={ manejarClick } />
+        <Boton
+        texto='Reiniciar'
+        esBotonDeClick={ false }
+        manejarClick={ reiniciarContador } />
+      </div>
     </div>
   );
 }
